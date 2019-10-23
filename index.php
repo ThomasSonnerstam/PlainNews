b0e552fd03210ba2b7e468c81da88ee2c459d264
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-10-23 13:26:49 +0200

    First try to print articles

--- index.php
+++ index.php
@@ -25,7 +25,48 @@ require __DIR__ . '/data.php';
             <p class="navparagraph">- The latest reports on planes for all you aviator fans!</p>
         </nav>
 
-        <section>
+        <section class="articlewrapper">
+
+            <?php foreach ($authors as $author) : ?>
+                <?php
+                    $title = $author["articles"][0]["title"];
+                    $image = $author["articles"][0]["image"];
+                    $content = $author["articles"][0]["content"];
+                    $authorname = $author["name"];
+                    $authorimage = $author["authorimage"];
+                    $published = $author["articles"][0]["published"];
+                    $likes = $author["articles"][0]["likes"];
+                    ?>
+                <article>
+                    <h3><?php echo $title; ?></h3>
+                    <img src="<?php echo $image; ?>" alt="">
+                    <p><?php echo $content; ?></p>
+                    <p><?php echo $authorname; ?></p>
+                    <img src="<?php echo $authorimage; ?>" alt="">
+                    <p><?php echo $published; ?></p>
+                    <p><?php echo $likes; ?></p>
+                </article>
+            <?php endforeach; ?>
+
+            <?php foreach ($authors as $author) : ?>
+                <?php
+                    $title = $author["articles"][1]["title"];
+                    $image = $author["articles"][1]["image"];
+                    $content = $author["articles"][1]["content"];
+                    $authorname = $author["name"];
+                    $authorimage = $author["authorimage"];
+                    $published = $author["articles"][1]["published"];
+                    $likes = $author["articles"][1]["likes"];
+                    ?>
+
+                <h3><?php echo $title; ?></h3>
+                <img src="<?php echo $image; ?>" alt="">
+                <p><?php echo $content; ?></p>
+                <p><?php echo $authorname; ?></p>
+                <img src="<?php echo $authorimage; ?>" alt="">
+                <p><?php echo $published; ?></p>
+                <p><?php echo $likes; ?></p>
+            <?php endforeach; ?>
 
 
 
