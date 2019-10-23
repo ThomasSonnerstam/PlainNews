4e4e0e6eed2acf5ab4f962ca7ae98412722fa043
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-10-23 10:17:45 +0200

    Boilerplate completed

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Plain News</title>
+    <link rel="stylesheet" href="https://unpkg.com/sanitize.css@11.0.0/sanitize.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,500i,700,700i&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="/Images/newspapericon.png" type="image/x-icon">
+</head>
+
+<body>
+
+
+
+
+
+</body>
+
+</html>
