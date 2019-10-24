b0a342c8f21ca1ac7f74e499d869b16756c9942d
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-10-24 15:24:03 +0200

    Updated index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require __DIR__ . '/functions.php';
 // Sorts the $articles array by the published date.
 
 usort($articles, function (array $a, array $b): int {
-    return strtotime($b["published"]) - strtotime($a["published"]);
+	return strtotime($b["published"]) - strtotime($a["published"]);
 });
 
 ?>
@@ -15,48 +15,52 @@ usort($articles, function (array $a, array $b): int {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Plain News</title>
-    <link rel="stylesheet" href="https://unpkg.com/sanitize.css@11.0.0/sanitize.css">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,500i,700,700i&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="/Images/newspapericon.png" type="image/x-icon">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Plain News</title>
+	<link rel="stylesheet" href="https://unpkg.com/sanitize.css@11.0.0/sanitize.css">
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,500i,700,700i&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" href="/Images/newspapericon.png" type="image/x-icon">
 </head>
 
 <body>
-    <main class="content-wrapper">
-        <nav>
-            <h1>Plane News</h1>
-            <p class="nav-paragraph">- The latest reports on planes for all you aviator fans!</p>
-        </nav>
+	<main class="content-wrapper">
+		<nav id="navtop">
+			<h1>Plane News</h1>
+			<p class="nav-paragraph">- The latest reports on planes for all you aviator fans!</p>
+		</nav>
 
-        <section class="article-wrapper">
+		<p>
+			<a class="scroll-to-top" href="#navtop">Back to the top!</a>
+		</p>
 
-            <?php foreach ($articles as $article) : ?>
-                <?php
-                    $title = $article["title"];
-                    $image = $article["image"];
-                    $content = $article["content"];
-                    $authorname = $authors[$article['authorId']]['name'];
-                    $authorImage = $authors[$article['authorId']]['authorImage'];
-                    $published = $article["published"];
-                    $likes = $article["likes"];
-                    ?>
-                <article>
-                    <h3><?php echo $title; ?></h3>
-                    <p>Published: <?php echo $published; ?></p>
-                    <img class="article-image" src="<?php echo $image; ?>" alt="Image of a plane">
-                    <p><?php echo nl2br($content); ?></p>
-                    <p class="author-name"><?php echo $authorname; ?></p>
-                    <img class="author-image" src="<?php echo $authorImage; ?>" alt="Image of <?php echo $authorname; ?>">
-                    <p><img class="like-icon" src="/Images/likeicon.png" alt=""> <?php echo randomLike(); ?></p>
-                </article>
-            <?php endforeach; ?>
+		<section class="article-wrapper">
 
-        </section>
-    </main>
+			<?php foreach ($articles as $i => $article) : ?>
+				<?php
+					$title = $article["title"];
+					$image = $article["image"];
+					$content = $article["content"];
+					$authorname = $authors[$article['authorId']]['name'];
+					$authorImage = $authors[$article['authorId']]['authorImage'];
+					$published = $article["published"];
+					$likes = $article["likes"];
+					?>
+				<article id="<?php echo $i; ?>">
+					<h3><?php echo $title; ?></h3>
+					<p>Published: <?php echo $published; ?></p>
+					<img class="article-image" src="<?php echo $image; ?>" alt="Image of a plane">
+					<p><?php echo nl2br($content); ?></p>
+					<p class="author-name"><?php echo $authorname; ?></p>
+					<img class="author-image" src="<?php echo $authorImage; ?>" alt="Image of <?php echo $authorname; ?>">
+					<p><img class="like-icon" src="/Images/likeicon.png" alt=""> <?php echo randomLike(); ?></p>
+				</article>
+			<?php endforeach; ?>
+
+		</section>
+	</main>
 </body>
 
 </html>
