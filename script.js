cb078b37c4bbdc2987bbf38060919b83f9011561
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-10-28 10:35:58 +0100

    Updates: - Minor changes in style.css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// the currentDate function prints out the current date in a "YYYY-MM-DD" format.
+// The currentDate function prints out the current date in a "YYYY-MM-DD" format.
 
 const date = new Date();
 const currentDate = `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()}`;
