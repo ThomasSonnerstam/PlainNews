f21b45db4fa15b5cbcfa161888f68a3a791b55ad
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-10-28 10:29:15 +0100

    Updates: - Description for the "currentDate" function in script.js added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// the currentDate function prints out the current date in a "YYYY-MM-DD" format.
+
 const date = new Date();
 const currentDate = `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()}`;
 
