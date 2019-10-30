3413e9b8162bb56e881aaa8fee328b619e4151a0
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-10-30 13:34:30 +0100

    Updates: - Added DocBlocks

--- functions.php
+++ functions.php
@@ -2,22 +2,39 @@
 
 declare(strict_types=1);
 
-// Generates a random number of "likes".
+
+/**
+ * Generates a random number of "likes".
+ *
+ * @return integer
+ */
 
 function randomLike(): int
 {
 	return rand(532, 13273);
 };
 
-// Sorts the $articles array by the published date.
+/**
+ * Sorts the $articles array by the published date.
+ *
+ * @param array $a
+ * @param array $b
+ * @return integer
+ */
 
 function sortDate(array $a, array $b): int
 {
 	return strtotime($b["published"]) <=> strtotime($a["published"]);
 };
 
-// A function that counts how many articles there are starting at 1 instead of 0.
-// The -1 is to limit the articles to the existing 10 articles instead of counting up to 11.
+/**
+ * A function that counts how many articles there are starting at 1 instead of 0.
+ * The -1 is to limit the articles to the existing 10 articles instead of counting up to 11.
+ *
+ * @param array $articles
+ * @param integer $i
+ * @return integer
+ */
 
 function countArticles(array $articles, int $i): int
 {
@@ -28,8 +45,15 @@ function countArticles(array $articles, int $i): int
 	}
 };
 
-// Prints a unique class name to the last article so that the last navigation
-// arrow can be formatted accordingly.
+/**
+ * Prints a unique class name to the last article so that the last navigation
+ * arrow can be formatted accordingly.
+ *
+ * @param array $articles
+ * @param integer $i
+ * @param string $stringToEcho
+ * @return void
+ */
 
 function echoOnLastArticle(array $articles, int $i, string $stringToEcho): void
 {
